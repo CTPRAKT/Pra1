924ce5708fa12fd6d82c43cbed9c4736f139a26b
Alexander <Random.heroes@yandex.ru>
2020-09-04 13:24:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog n1 = new Dog ("Stan",5);
+        Dog n2 = new Dog ("Flash", 2);
+        Dog n3 = new Dog ("Kent",8);
+        System.out.println(n1);
+        n1.humanAge();
+        n2.humanAge();
+        n3.humanAge();
+    }
+}
